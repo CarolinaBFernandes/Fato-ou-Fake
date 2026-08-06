3277230e5f39c8f79dd2e281dd536ab68f8c414a
BrendaLopes03 <165107013+BrendaLopes03@users.noreply.github.com>
2026-08-06 18:35:34 -0300

    Adicionada explicações para as perguntas verdadeiras

--- lib/data/perguntas.dart
+++ lib/data/perguntas.dart
@@ -75,59 +75,69 @@ final List<Pergunta> bancoPerguntas = [
     imagem: "assets/barcelos.png",
     descricao: "Sábado, dia 04/04/2026 inaugura em Valão do Barro a Drogaria Barcelos.",
     resposta: true,
+    explicacao: "Fato. Notícia publicado no aplicativo de notícias RCL (Midi) por Maria das Graças, moradora de Valão do Barro.",
   ),
 
   Pergunta(
     imagem: "assets/politica.png",
     descricao: "São Sebastião do Alto se destaca no RJ com representação feminina em meio a baixa presença de mulheres na política brasileira.",
     resposta: true,
+    explicacao:" Fato. A cidade possui sim uma forte representação de mulheres na política. Postado no aplicativo de notícias RCL (Midi) por Beatriz Siqueira integrante do Projeto."
   ),
 
   Pergunta(
     imagem: "assets/exame.png",
     descricao: "Ótica Pontilhão realizou no dia 09/04 exames de vista gratuitos para a população de Valão do Barro.",
     resposta: true,
+    explicacao: "Fato. notícia publicada no aplicativo de notícias RCL (Midi) por Maria das Graças moradora de Valão do Barro",
   ),
 
   Pergunta(
     imagem: "assets/tombou.png",
     descricao: "Um caminhão tombou na RJ-116, na altura de Valão do Barro, por volta das 19h de terça (19/05). O acidente mobilizou equipes de atendimento rodoviário. O motorista foi socorrido após o tombamento e encaminhado ao hospital de São Sebastião do Alto.",
     resposta: true,
+    explicacao: " Fato. Notícia retirada do site Serra News. ",
   ),
 
   Pergunta(
     imagem: "assets/expo.png",
     descricao: "Valão do Barro será palco da 2ª Expo Gospel nos dias 7 e 8 de agosto. Promovido pela Prefeitura Municipal, em parceria com a Associação de Pastores Evangélicos de São Sebastião do Alto, o evento terá dois dias de programação voltada ao louvor, à adoração e à celebração da fé cristã.",
     resposta: true,
+    explicacao: "Fato. Notícia retirada do site O Dia e confirmada no Instagram da Cidade.",
   ),
 
   Pergunta(
     imagem: "assets/comemora.png",
     descricao: "São Sebastião do Alto comemora seus 135 anos em 17 de abril de 2026.",
     resposta: true,
+    explicacao: "Fato. Notícia tirada do aplicativo de notícias RCL (Midi), escrita por Maria das Graças moradora e Valão do Barro. ",
   ),
 
   Pergunta(
     imagem: "assets/bullying.png",
     descricao: "O colégio Estadual Januário de Toledo Pizza realizou a semana de conscientização sobre o combate ao bullying.",
     resposta: true,
+    explicacao: " Fato. Notícia tirada do aplicativo de notícias RCL (Midi), escrita por Maria das Graças moradora de Valão do Barro.",
   ),
 
   Pergunta(
     imagem: "assets/faa.png",
     descricao: "A FAA PET realizou castrações gratuitas no Município de São Sebastião do Alto, nos dias 07 e 08 de abril de 2026, na quadra municipal de Valão do Barro.",
     resposta: true,
+    explicacao: " Fato. Notícia tirada do aplicativo de notícias RCL (Midi), escrita por Maria das Graças",
   ),
 
   Pergunta(
     imagem: "assets/chuvas.png",
     descricao: "Campanhas mobilizam São Sebastião do Alto. Doações são para famílias atingidas pelas fortes chuvas.",
     resposta: true,
+    explicacao: " Fato. tirado do aplicativo de notícias RCL (Midi), escrita por Jacqueline Deolindo, orientadora do projeto. ",
   ),
 
   Pergunta(
     imagem: "assets/vacina.png",
     descricao: "A Prefeitura de São Sebastião do Alto, por meio da Secretaria de Saúde e Higiene, informa que a nova vacina pneumocócica 20-valente já está disponível em nossa rede municipal. Dirija-se à unidade de vacinação mais próxima de sua residência. As salas de vacinação funcionam de segunda a sexta-feira, das 08h às 16h.",
     resposta: true,
+    explicacao: "Fato. Tirado do Instagram de São Sebastião do Alto.",
   ),
 ];
